f11a180bea3f29ebd4b84ac5adfebd2edd925a4e
am52saif <aliasgar.s.merchant@gmail.com>
2016-07-02 01:21:49 +0400

    Added a Mock Up Data of Facts

--- app/src/main/java/com/aliasgar/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/aliasgar/funfacts/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.aliasgar.funfacts;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -11,33 +13,26 @@ import java.util.Random;
 public class FunFactsActivity extends AppCompatActivity {
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private RelativeLayout mRelativeLayout;
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
-//        mFactTextView = (TextView) findViewById(R.id.factTextView);
-//        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+        mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 
         mShowFactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String fact;
-                String[] facts = {
-                        "Ants stretch when they wake up in the morning.",
-                        "Ostriches can run faster than horses.",
-                        "Olympic gold medals are actually made mostly of silver.",
-                        "You are born with 300 bones; by the time you are an adult you will have 206.",
-                        "It takes about 8 minutes for light from the Sun to reach Earth.",
-                        "Some bamboo plants can grow almost a meter in just one day.",
-                        "The state of Florida is bigger than England.",
-                        "Some penguins can leap 2-3 meters out of the water.",
-                        "On average, it takes 66 days to form a new habit.",
-                        "Mammoths still walked the earth when the Great Pyramid was being built." };
-
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                fact = facts[randomNumber];
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
                 mFactTextView.setText(fact);
+                mRelativeLayout.setBackgroundColor(color);
+                mShowFactButton.setTextColor(color);
             }
         });
     }
